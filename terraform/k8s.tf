410f23e46569dd2e80afd872953dbadb3cf42cd8
OrHazut <orhazut123@gmail.com>
2024-06-28 22:47:04 +0300

    shkib

---
+++ terraform/k8s.tf
@@ -0,0 +1,85 @@
+# Define the Namespace
+resource "kubernetes_namespace" "bluered" {
+  metadata {
+    name = "bluered"
+  }
+}
+
+# # Define the ConfigMap
+# resource "kubernetes_config_map" "config_color" {
+#   metadata {
+#     name      = "config-color"
+#     namespace = kubernetes_namespace.bluered.metadata[0].name
+#   }
+#
+#   data = {
+#     X = "1"
+#   }
+# }
+
+# Define the Deployment
+resource "kubernetes_deployment" "bluered" {
+  metadata {
+    name      = "bluered"
+    namespace = kubernetes_namespace.bluered.metadata[0].name
+
+    labels = {
+      app = "bluered"
+    }
+  }
+
+  spec {
+    replicas = 1
+
+    selector {
+      match_labels = {
+        app = "bluered"
+      }
+    }
+
+    template {
+      metadata {
+        labels = {
+          app = "bluered"
+        }
+      }
+
+      spec {
+        container {
+          name  = "bluered"
+          image = "orhaz/bluered:v1.0"
+
+#           env_from {
+#             config_map_ref {
+#               name = kubernetes_config_map.config_color.metadata[0].name
+#             }
+#           }
+
+          resources {}
+        }
+      }
+    }
+  }
+}
+
+# Define the Service
+resource "kubernetes_service" "bluered" {
+  metadata {
+    name      = "bluered"
+    namespace = kubernetes_namespace.bluered.metadata[0].name
+  }
+
+  spec {
+    selector = {
+      app = "bluered"
+    }
+
+    port {
+      protocol   = "TCP"
+      port        = 80
+      target_port = 8080
+    }
+
+    type = "LoadBalancer"
+  }
+}
--- terraform/providers.tf
+++ terraform/providers.tf
@@ -19,8 +19,16 @@ terraform {
 #   }
 # }
 
+data "aws_eks_cluster_auth" "eks_cluster_auth" {
+  depends_on = [aws_eks_cluster.eks-cluster]
+  name       = aws_eks_cluster.eks-cluster.name
+}
+
 provider "kubernetes" {
   config_path = "~/.kube/config"
+  host                   = data.aws_eks_cluster.eks-cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks-cluster.certificate_authority[0].data)
+  token                  = data.aws_eks_cluster_auth.eks_cluster_auth.token
 }
 
 provider "aws" {
