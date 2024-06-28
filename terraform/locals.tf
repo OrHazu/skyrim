814c07d4c2fd4cc13e66f7ccfdca055dd6ca86fb
OrHazut <orhazut123@gmail.com>
2024-06-28 18:24:27 +0300

    shkib

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -6,6 +6,6 @@ data "aws_eks_cluster" "eks-cluster" {
   name = aws_eks_cluster.eks-cluster.name
 }
 
-locals {
-  oidc = trimprefix(data.aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer, "https://")
-}
+# locals {
+#   oidc = trimprefix(data.aws_eks_cluster.eks-cluster.identity[0].oidc[0].issuer, "https://")
+# }
